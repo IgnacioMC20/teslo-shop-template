48838d2d95f077fcc79326653ce8253925e2155e
ignaciocuyun <ignacio.cuyun@telus.com>
2022-10-12 11:28:20 -0600

    light-theme modified

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import '@fontsource/roboto/300.css';
 import { ThemeProvider } from '@mui/system';
+import { CssBaseline } from '@mui/material';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
   )
